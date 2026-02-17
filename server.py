bf64763e0bbacdcc50184d5ceaf0f3aadc0a25c2
Pavuch0k <gubanovnikita783@gmail.com>
2026-02-17 22:56:00 +0400

    Добавлен HTTP-сервер

---
+++ server.py
@@ -0,0 +1,21 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain; charset=utf-8')
+            self.end_headers()
+            self.wfile.write(b'Hello from Effective Mobile!')
+        else:
+            self.send_error(404)
+
+    def log_message(self, format, *args):
+        pass
+
+
+if __name__ == '__main__':
+    server = HTTPServer(('0.0.0.0', 8080), Handler)
+    print('Server running on http://0.0.0.0:8080')
+    server.serve_forever()
